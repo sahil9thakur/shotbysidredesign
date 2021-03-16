a24806d5c8b85da3f21d3442a7429a12bc863ed3
Sahil Thakur <thakursahil92@gmail.com>
2021-03-15 21:32:58 -0400

    enabled email logging

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://api.7starcanada.com";
 
 ReactDOM.render(
     <Provider store={store}>
